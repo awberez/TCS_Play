3c5abc633afe68457f6db3e5d1809fcf932d3c59
awberez <alex@berez.com>
2020-05-21 17:10:29 -0500

    updated demo generator to include review

--- public/js/app.js
+++ public/js/app.js
@@ -9,22 +9,20 @@ $(function(){
 				match_id: match,
 				white_player: {id: 123, username: "WhitePlayerName", uuid: generateUUID()},
 				black_player: {id: 321, username: "BlackPlayerName", uuid: generateUUID()},
-				coaches: [
-					{id: 101, username: "FirstCoachName", uuid: generateUUID()}, 
-					{id: 202, username: "SecondCoachName", uuid: generateUUID()}
-				]
+				coaches: [ {id: 101, username: "CoachName", uuid: generateUUID()} ]
 			}
 			$.post(`/newgame`, data, (res)=>{ 
 				if (res == "success") {
-					let matchUrl = `${window.location.href.slice(0, -9)}match/`, observeUrl = `${window.location.href.slice(0, -9)}observe/${match}/`;
-					$("#gameLinks").empty().append(`
-						<a id="whiteLink" href=${matchUrl}${data.white_player.uuid} target="blank">White Player Link</a><p>Copyable URL: ${matchUrl}${data.white_player.uuid}</p>
-						<a id="blackLink" href=${matchUrl}${data.black_player.uuid} target="blank">Black Player Link</a><p>Copyable URL: ${matchUrl}${data.black_player.uuid}</p>
-						<a id="blackLink" href=${matchUrl}${data.coaches[0].uuid}   target="blank">Coach 1 Link</a>     <p>Copyable URL: ${matchUrl}${data.coaches[0].uuid}</p>
-						<a id="blackLink" href=${matchUrl}${data.coaches[1].uuid}   target="blank">Coach 2 Link</a>     <p>Copyable URL: ${matchUrl}${data.coaches[1].uuid}</p>
-						<a id="blackLink" href=${observeUrl}444/FirstObserver  target="blank">Observer 1 Link</a><p>Copyable URL: ${observeUrl}444/FirstObserver</p>
-						<a id="blackLink" href=${observeUrl}555/SecondObserver target="blank">Observer 2 Link</a><p>Copyable URL: ${observeUrl}555/SecondObserver</p>
-						<a id="blackLink" href=${observeUrl}666/ThirdObserver  target="blank">Observer 3 Link</a><p>Copyable URL: ${observeUrl}666/ThirdObserver</p>
+					let matchUrl = `${window.location.href.slice(0, -9)}match/`, observeUrl = `${window.location.href.slice(0, -9)}observe/${match}/`,
+					reviewUrl = `${window.location.href.slice(0, -9)}review/match/`, reviewCoachUrl = `${window.location.href.slice(0, -9)}review/coach/`
+					$("#demoContent").empty().append(`
+						<br/><br/>
+						<a href=${matchUrl}${data.white_player.uuid} target="blank">White Player</a><p>Copyable URL: ${matchUrl}${data.white_player.uuid}</p>
+						<a href=${matchUrl}${data.black_player.uuid} target="blank">Black Player</a><p>Copyable URL: ${matchUrl}${data.black_player.uuid}</p>
+						<a href=${matchUrl}${data.coaches[0].uuid} target="blank">Coach</a><p>Copyable URL: ${matchUrl}${data.coaches[0].uuid}</p>
+						<a href=${observeUrl}444/ObserverName target="blank">Observer</a><p>Copyable URL: ${observeUrl}444/ObserverName</p>
+						<a href=${reviewCoachUrl}${data.coaches[0].uuid} target="blank">Game Review Coach (game must be ended first)</a><p>Copyable URL: ${reviewCoachUrl}${data.coaches[0].uuid}</p>
+						<a href=${reviewUrl}${match} target="blank">Game Review Generic (game must be ended first)</a><p>Copyable URL: ${reviewUrl}${match}</p>
 					`);
 				}
 				else { getGameLinks(); };
